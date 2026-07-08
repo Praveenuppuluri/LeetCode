03865ee34d78039289936f33371aacff138ccb20
Praveenuppuluri <166889741+Praveenuppuluri@users.noreply.github.com>
2026-07-08 16:10:58 +0530

    Time: 0 ms (100%), Space: 48.6 MB (14.11%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,19 +1,50 @@
+import java.util.Scanner;
+
 class Solution {
-    public int search(int[] nums, int target) {
-        int low=0;
-        int i;
-        int high=nums.length-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(target==nums[mid]){
+
+    public static int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
                 return mid;
-            }else if(target>nums[mid]){
-                low=mid+1;
-            }else if(target<nums[mid]){
-                high=mid-1;
+            } else if (target > nums[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
-            
         }
+
         return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter the sorted array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the target element: ");
+        int target = sc.nextInt();
+
+        int result = search(nums, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+
+        sc.close();
     }
 }
